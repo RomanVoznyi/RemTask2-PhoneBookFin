502a25655c1010917d31412b10c245fe8e1d8628
RomanVoznyy <roman.voznyy.52@gmail.com>
2021-12-11 00:18:11 +0200

    added styles for contacts

--- src/Components/AddForm.js
+++ src/Components/AddForm.js
@@ -1,5 +1,7 @@
 import { useState } from 'react';
 import { nanoid } from 'nanoid';
+import { AiOutlinePlusCircle, AiOutlineMinusCircle } from 'react-icons/ai';
+import { toast } from 'react-toastify';
 
 const INITIAL_STATE = {
   name: '',
@@ -9,9 +11,12 @@ const INITIAL_STATE = {
 const AddForm = ({ contacts, setContacts }) => {
   const [name, setName] = useState(INITIAL_STATE.name);
   const [number, setNumber] = useState(INITIAL_STATE.number);
+  const [open, setOpen] = useState(false);
 
   const handleInputField = ({ target }) =>
     target.name === 'number' ? setNumber(target.value) : setName(target.value);
+
+  const toggleAddForm = () => setOpen(prevState => !prevState);
 
   const handleSubmit = evt => {
     evt.preventDefault();
@@ -22,42 +27,59 @@ const AddForm = ({ contacts, setContacts }) => {
       }
     }
     setContacts(prevState => [...prevState, { id: nanoid(), name, number }]);
+    toast.success(`Contact was successfuly added!`);
     setName(INITIAL_STATE.name);
     setNumber(INITIAL_STATE.number);
+    toggleAddForm();
   };
 
   return (
-    <form className="form" onSubmit={handleSubmit}>
-      <label className="labelNameField">
-        <span className="titleNameField">Name</span>
-        <input
-          type="text"
-          className="nameField"
-          name="name"
-          pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-          title="Имя может состоять только из букв, апострофа, тире и пробелов."
-          value={name}
-          onChange={handleInputField}
-          required
-        />
-      </label>
-      <label className="labelNumberField">
-        <span className="titleNumberField">Number</span>
-        <input
-          type="tel"
-          className="numberField"
-          name="number"
-          pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-          title="Номер телефона должен состоять цифр и может содержать пробелы, тире, круглые скобки и может начинаться с +"
-          value={number}
-          onChange={handleInputField}
-          required
-        />
-      </label>
-      <button type="submit" className="addButton">
-        Add contact
-      </button>
-    </form>
+    <div className="addBox">
+      <div className="addIconBox" onClick={toggleAddForm}>
+        {!open ? (
+          <AiOutlinePlusCircle className="addIcon" />
+        ) : (
+          <AiOutlineMinusCircle className="addIcon" />
+        )}
+        <span className="addIconTitle">Add new contact</span>
+      </div>
+
+      {open && (
+        <form className="addForm" onSubmit={handleSubmit}>
+          <label className="addLabel">
+            <input
+              type="text"
+              className="addInput"
+              name="name"
+              pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+              title="Имя может состоять только из букв, апострофа, тире и пробелов."
+              value={name}
+              onChange={handleInputField}
+              placeholder=" "
+              required
+            />
+            <span className="addTitle">Name</span>
+          </label>
+          <label className="addLabel">
+            <input
+              type="tel"
+              className="addInput"
+              name="number"
+              pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+              title="Номер телефона должен состоять цифр и может содержать пробелы, тире, круглые скобки и может начинаться с +"
+              value={number}
+              onChange={handleInputField}
+              placeholder=" "
+              required
+            />
+            <span className="addTitle">Number</span>
+          </label>
+          <button type="submit" className="addButton">
+            Add contact
+          </button>
+        </form>
+      )}
+    </div>
   );
 };
 
--- src/Components/Contacts.js
+++ src/Components/Contacts.js
@@ -1,23 +1,29 @@
 import { BsPersonCircle } from 'react-icons/bs';
+import { TiDeleteOutline } from 'react-icons/ti';
 
 const Contacts = ({ contacts, filteredArray, deleteContact }) => {
   const handleClick = e => deleteContact(e.target.dataset.id);
 
   return (
-    <ul>
+    <ul className="contactList">
       {contacts.length > 0 &&
         filteredArray(contacts).map(el => (
-          <li key={el.id} className="listItem">
-            <BsPersonCircle style={{ color: '#000099' }} /> -{' '}
-            <span>{el.name}</span>: <span>{el.number}</span>
-            <button
-              type="button"
-              data-id={el.id}
-              className="deleteButton"
-              onClick={handleClick}
-            >
-              Delete
-            </button>
+          <li key={el.id} className="contactListItem">
+            <div className="contactIconBox">
+              <BsPersonCircle className="contactIcon" />
+            </div>
+            <div className="contactNameBox">
+              <h3 className="contactName">{el.name}</h3>
+              <span className="contactPhone">{el.number}</span>
+              <button
+                type="button"
+                data-id={el.id}
+                className="deleteButton"
+                onClick={handleClick}
+              >
+                <TiDeleteOutline className="deleteIcon" />
+              </button>
+            </div>
           </li>
         ))}
     </ul>
